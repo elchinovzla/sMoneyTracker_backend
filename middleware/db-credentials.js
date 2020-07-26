597c14e70c25037443551ea023e1797320d85662
Sonny Ng <sonnyng.vzla@gmail.com>
2020-07-26 03:23:28 -0600

    add missing files

---
+++ middleware/db-credentials.js
@@ -0,0 +1,3 @@
+const USER_NAME = 'sonnyng';
+const PASSWORD = 'q1w2e3r4';
+module.exports = { USER_NAME, PASSWORD };
---
+++ middleware/email-sender-credentials.js
@@ -0,0 +1,3 @@
+const USER_NAME = 'sonnyng.business@gmail.com';
+const PASSWORD = 'nitke4-giwxud-kIrtux';
+module.exports = { USER_NAME, PASSWORD};
