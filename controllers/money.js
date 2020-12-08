3fec529a93c4710f48827187a88fbda199f9c82d
Sonny Ng <sonnyng.vzla@gmail.com>
2020-12-07 18:14:45 -0700

    Add money group type to keep track of money invested

--- controllers/money.js
+++ controllers/money.js
@@ -3,6 +3,7 @@ const Dinero = require('dinero.js');
 Dinero.defaultCurrency = 'CAD';
 const MONEY_TYPE = {
   MONEY_IN_THE_BANK: 'MONEY_IN_THE_BANK',
+  MONEY_INVESTED: 'MONEY_INVESTED',
   UNCLAIMED: 'UNCLAIMED',
   OTHER: 'OTHER',
   CHECKING: 'CHECKING',
@@ -10,8 +11,8 @@ const MONEY_TYPE = {
   RRSP: 'RRSP',
   TFSA: 'TFSA',
   CASH: 'CASH',
-  GIFT_CARD: 'GIFT_CARD'
-}
+  GIFT_CARD: 'GIFT_CARD',
+};
 
 exports.createMoney = (req, res, next) => {
   const money = new Money({
@@ -19,19 +20,19 @@ exports.createMoney = (req, res, next) => {
     moneyType: req.body.moneyType,
     amount: req.body.amount,
     note: req.body.note,
-    createdById: req.body.createdById
+    createdById: req.body.createdById,
   });
   money
     .save()
-    .then(result => {
+    .then((result) => {
       res.status(201).json({
         message: 'Money created',
-        result: result
+        result: result,
       });
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json({
-        message: 'Error creating money: ' + error
+        message: 'Error creating money: ' + error,
       });
     });
 };
@@ -40,119 +41,111 @@ exports.getMoneyEntries = (req, res, next) => {
   const pageSize = +req.query.pagesize;
   const currentPage = +req.query.page;
   const moneyQuery = Money.find({
-    createdById: req.query.createdById
+    createdById: req.query.createdById,
   }).sort([['description', 1]]);
   if (pageSize && currentPage) {
     moneyQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
   }
   moneyQuery
-    .then(moneyEntries => {
+    .then((moneyEntries) => {
       res.status(200).json({
         message: 'Money fetched successfully',
-        moneyEntries: moneyEntries
+        moneyEntries: moneyEntries,
       });
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json({
-        message: 'Failed to get money: ' + error
+        message: 'Failed to get money: ' + error,
       });
     });
 };
 
 exports.getTotalMoneyRecords = (req, res, next) => {
   Money.find({
-    createdById: req.params.createdById
-  }).then(moneyEntries => {
+    createdById: req.params.createdById,
+  }).then((moneyEntries) => {
     if (moneyEntries) {
       res.status(200).json({
-        moneyTotalCount: getTotal(moneyEntries)
-      })
+        moneyTotalCount: getTotal(moneyEntries),
+      });
     } else {
       res.status(200).json({
-        moneyTotalCount: 0
-      })
+        moneyTotalCount: 0,
+      });
     }
-  })
+  });
 };
 
 exports.getMoney = (req, res, next) => {
   Money.findById(req.params.id)
-    .then(moneyEntries => {
+    .then((moneyEntries) => {
       if (moneyEntries) {
         res.status(200).json(moneyEntries);
       } else {
         res.status(400).json({
-          message: 'Money not found'
+          message: 'Money not found',
         });
       }
-    }).catch(error => {
+    })
+    .catch((error) => {
       res.status(500).json({
-        message: 'Failed to get money: ' + error
-      })
+        message: 'Failed to get money: ' + error,
+      });
     });
 };
 
 exports.getMoneyInfo = (req, res, next) => {
   Money.find({
-    createdById: req.params.id
-  }).then(moneyEntries => {
-    if (moneyEntries) {
-      res.status(200).json({
-        moneyInTheBank: getTotalMoneyAmount(
-          moneyEntries,
-          MONEY_TYPE.MONEY_IN_THE_BANK
-        ),
-        moneyUnclaimed: getTotalMoneyAmount(
-          moneyEntries,
-          MONEY_TYPE.UNCLAIMED
-        ),
-        moneyOther: getTotalMoneyAmount(
-          moneyEntries,
-          MONEY_TYPE.OTHER
-        ),
-        moneyInChecking: getTotalMoneyAmount(
-          moneyEntries,
-          MONEY_TYPE.CHECKING
-        ),
-        moneyInSavings: getTotalMoneyAmount(
-          moneyEntries,
-          MONEY_TYPE.SAVINGS
-        ),
-        moneyInRrsp: getTotalMoneyAmount(
-          moneyEntries,
-          MONEY_TYPE.RRSP
-        ),
-        moneyInTfsa: getTotalMoneyAmount(
-          moneyEntries,
-          MONEY_TYPE.TFSA
-        ),
-        moneyCash: getTotalMoneyAmount(
-          moneyEntries,
-          MONEY_TYPE.CASH
-        ),
-        moneyGiftCard: getTotalMoneyAmount(
-          moneyEntries,
-          MONEY_TYPE.GIFT_CARD
-        )
-      });
-    } else {
-      res.status(200).json({
-        message: 'Could not find any money entry for user',
-        moneyInTheBank: 0,
-        moneyUnclaimed: 0,
-        moneyOther: 0,
-        moneyInChecking: 0,
-        moneyInSavings: 0,
-        moneyInRrsp: 0,
-        moneyInTfsa: 0,
-        moneyCash: 0,
-        moneyGiftCard: 0
-      });
-    }
+    createdById: req.params.id,
   })
-    .catch(error => {
+    .then((moneyEntries) => {
+      if (moneyEntries) {
+        res.status(200).json({
+          moneyInTheBank: getTotalMoneyAmount(
+            moneyEntries,
+            MONEY_TYPE.MONEY_IN_THE_BANK
+          ),
+          moneyInvested: getTotalMoneyAmount(
+            moneyEntries,
+            MONEY_TYPE.MONEY_INVESTED
+          ),
+          moneyUnclaimed: getTotalMoneyAmount(
+            moneyEntries,
+            MONEY_TYPE.UNCLAIMED
+          ),
+          moneyOther: getTotalMoneyAmount(moneyEntries, MONEY_TYPE.OTHER),
+          moneyInChecking: getTotalMoneyAmount(
+            moneyEntries,
+            MONEY_TYPE.CHECKING
+          ),
+          moneyInSavings: getTotalMoneyAmount(moneyEntries, MONEY_TYPE.SAVINGS),
+          moneyInRrsp: getTotalMoneyAmount(moneyEntries, MONEY_TYPE.RRSP),
+          moneyInTfsa: getTotalMoneyAmount(moneyEntries, MONEY_TYPE.TFSA),
+          moneyCash: getTotalMoneyAmount(moneyEntries, MONEY_TYPE.CASH),
+          moneyGiftCard: getTotalMoneyAmount(
+            moneyEntries,
+            MONEY_TYPE.GIFT_CARD
+          ),
+        });
+      } else {
+        res.status(200).json({
+          message: 'Could not find any money entry for user',
+          moneyInTheBank: 0,
+          moneyInvested: 0,
+          moneyUnclaimed: 0,
+          moneyOther: 0,
+          moneyInChecking: 0,
+          moneyInSavings: 0,
+          moneyInRrsp: 0,
+          moneyInTfsa: 0,
+          moneyCash: 0,
+          moneyGiftCard: 0,
+        });
+      }
+    })
+    .catch((error) => {
       res.status(500).json({
-        message: 'Failed to get a Money Info: ' + error
+        message: 'Failed to get a Money Info: ' + error,
       });
     });
 };
@@ -164,29 +157,31 @@ exports.updateMoney = (req, res, next) => {
       description: req.body.description,
       moneyType: req.body.moneyType,
       amount: req.body.amount,
-      note: req.body.note
+      note: req.body.note,
     }
-  ).then(() => {
-    res.status(201).json({
-      message: 'Money updated'
+  )
+    .then(() => {
+      res.status(201).json({
+        message: 'Money updated',
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: 'Failed to update money: ' + error,
+      });
     });
-  }).catch(error => {
-    res.status(500).json({
-      message: 'Failed to update money: ' + error
-    });
-  });
 };
 
 exports.deleteMoney = (req, res, next) => {
   Money.deleteOne({ _id: req.params.id })
     .then(() => {
       res.status(201).json({
-        message: 'Money deleted'
+        message: 'Money deleted',
       });
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json({
-        message: 'Failed to delete money: ' + error
+        message: 'Failed to delete money: ' + error,
       });
     });
 };
@@ -201,27 +196,39 @@ function getDinero(moneyEntries, moneyType) {
     }
   });
   return totalAmount;
-};
+}
 
 function getTotalMoneyAmount(moneyEntries, moneyType) {
   let totalAmount = Dinero({ amount: 0 });
   switch (moneyType) {
     case MONEY_TYPE.MONEY_IN_THE_BANK: {
-      totalAmount = totalAmount.add(getDinero(moneyEntries, MONEY_TYPE.CHECKING));
-      totalAmount = totalAmount.add(getDinero(moneyEntries, MONEY_TYPE.SAVINGS));
+      totalAmount = totalAmount.add(
+        getDinero(moneyEntries, MONEY_TYPE.CHECKING)
+      );
+      totalAmount = totalAmount.add(
+        getDinero(moneyEntries, MONEY_TYPE.SAVINGS)
+      );
+      break;
+    }
+    case MONEY_TYPE.MONEY_INVESTED: {
       totalAmount = totalAmount.add(getDinero(moneyEntries, MONEY_TYPE.RRSP));
       totalAmount = totalAmount.add(getDinero(moneyEntries, MONEY_TYPE.TFSA));
+      break;
     }
     case MONEY_TYPE.OTHER: {
       totalAmount = totalAmount.add(getDinero(moneyEntries, MONEY_TYPE.CASH));
-      totalAmount = totalAmount.add(getDinero(moneyEntries, MONEY_TYPE.GIFT_CARD));
+      totalAmount = totalAmount.add(
+        getDinero(moneyEntries, MONEY_TYPE.GIFT_CARD)
+      );
+      break;
     }
     default: {
       totalAmount = totalAmount.add(getDinero(moneyEntries, moneyType));
+      break;
     }
   }
   return totalAmount.getAmount() / 100;
-};
+}
 
 function getTotal(moneyEntries) {
   let totalCount = 0;
@@ -229,4 +236,4 @@ function getTotal(moneyEntries) {
     totalCount++;
   });
   return totalCount;
-};
+}
